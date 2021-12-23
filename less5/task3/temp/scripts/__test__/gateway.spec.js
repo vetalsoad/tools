2827400e349b180ae1830463d0726183e24460a1
vetal <vetalsoad@gmail.com>
2021-12-23 10:49:14 +0200

    rr

---
+++ less5/task3/temp/scripts/__test__/gateway.spec.js
@@ -0,0 +1,6 @@
+import { getTasksList } from "../tasksGateway";
+it("should return promise pending", () => {
+  getTasksList().then(tasks => {
+    expect(Array.isArray(tasks).toEqual(true));
+  });
+});
---
+++ less5/task3/temp/scripts/todoList.js
@@ -0,0 +1,8 @@
+import { onCreateTask } from "./createTask.js";
+import { onToggleTask } from "./updateTask.js";
+export const initTodoListHandlers = () => {
+  const createBtnELem = document.querySelector(".create-task-btn");
+  createBtnELem.addEventListener("click", onCreateTask);
+  const todoListElem = document.querySelector(".list");
+  todoListElem.addEventListener("click", onToggleTask);
+};
