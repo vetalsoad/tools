7b5ad7979383a635bf5c93a0969fc2fc961ee500
vetal <vetalsoad@gmail.com>
2021-12-23 16:28:59 +0200

    4

--- less7/task2/webpack.config.js
+++ less7/task2/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -32,6 +33,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),                       
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
